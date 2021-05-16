4f5972811bce14ba61cbfd91c97c9a30749ea123
max-java <6666350@gmail.com>
2021-05-16 16:33:47 +0300

    update structure

--- aTemplate/src/main/java/by/jrr/start/Application.java
+++ aTemplate/src/main/java/by/jrr/start/Application.java
@@ -1,9 +1,8 @@
-package com.tutrit.java.quickstart;
+package by.jrr.start;
 
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World");
     }
 }
-
