d4bc36d27e77cc13adfd72ce3116aae5342d9b92
TsSlava <74265860+TsSlava@users.noreply.github.com>
2021-05-29 22:11:55 +0300

    Some changes) Slava Test, Hello)

--- aTemplate/src/main/java/by/jrr/start/Application.java
+++ aTemplate/src/main/java/by/jrr/start/Application.java
@@ -7,5 +7,6 @@ public class Application {
     public static void main(String[] args) {
         Logger log = Logger.getLogger("Test");
         log.info("Hello world");
+        log.info("Hello everyone");
     }
 }
