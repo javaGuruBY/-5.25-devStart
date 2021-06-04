ea31a6987af0239a74634d6c65cc6e1376f1d0b5
hillel-j2ee <hillel-j2ee@ukr.net>
2021-06-04 15:53:28 +0300

    FlipCoinVoloshchenko

--- aTemplate/src/main/java/voloshchenkoHomeWork/Application.java
+++ aTemplate/src/main/java/voloshchenkoHomeWork/Application.java
@@ -4,13 +4,14 @@ import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("Монета подброшена! ");
         Random random = new Random();
         int flip = random.nextInt(2);
         if(flip == 1){
-            System.out.println("Orel");
+            System.out.println("Выпал: Orel");
         }
         else{
-            System.out.println("Reshka");
+            System.out.println("Выпала: Reshka");
         }
     }
 }
